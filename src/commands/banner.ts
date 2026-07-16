5b9672189fd78b5cbe113abe20d7d8d96591c7ef
glaklandy <ijustexist@hotmail.com>
2026-07-16 16:00:45 +0100

    missed a semi-colon...

--- src/commands/banner.ts
+++ src/commands/banner.ts
@@ -1,22 +1,22 @@
 import command from '../../config.json' with {type: 'json'};
 
 const createBanner = () : string[] => {
-  const banner : string[] = [];
-  banner.push("<br>")
-  command.ascii.forEach((ele) => {
-    let bannerString = "";
-    //this is for the ascii art
-    for (let i = 0; i < ele.length; i++) {
-      if (ele[i] === " ") {
-        bannerString += "&nbsp;";
-      } else {
-        bannerString += ele[i];
-      }
-    }
-    
-    let eleToPush = `<pre>${bannerString}</pre>`;
-    banner.push(eleToPush);
-  });
+    const banner : string[] = [];
+    banner.push("<br>")
+    command.ascii.forEach((ele) => {
+        let bannerString = "";
+        //this is for the ascii art
+        for (let i = 0; i < ele.length; i++) {
+        if (ele[i] === " ") {
+            bannerString += "&nbsp;";
+        } else {
+            bannerString += ele[i];
+        }
+        }
+        
+        let eleToPush = `<pre>${bannerString}</pre>`;
+        banner.push(eleToPush);
+    });
   
 /*    
     command.bannerText.forEach((line) => {
@@ -39,12 +39,13 @@ const createBanner = () : string[] => {
     banner.push("<br><br>");
     banner.push("software solutions 4 u!");
     banner.push("mainly MIDI-related (so maybe not 4 u at all)");
-    banner.push("and it might be a website for a software developer, but don't judge on the website, it's just a bit o' fun")
+    banner.push("and it might be a website for a software developer, but don't judge on the website, it's just a bit o' fun");
     banner.push("<br>");
-    banner.push("use the \"shell\" below to navigate the website (or be boring and use the mouse - if you dare...)");
+    banner.push("use the \"shell\" below to navigate the website (or be boring and use the mouse)");
     banner.push("type <span class='command'>help</span> for a list of available commands");
     banner.push(`type <span class='command'>repo</span> to view the GitHub repository or click <a href='${command.repoLink}' target='_blank'>here</a>.`);
     banner.push("<br><br>");
+    
   return banner;
 }
 
